b6137d6048df0d73ef9d7b01f6aca81e09cc27e3
liuxinxin <2861706215@qq.com>
2019-10-15 09:06:14 +0800

    work

---
+++ build/precache-manifest.082f62f44a4c12829c908bfaf398322b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2470cd533c6d3e9592fbc4c70cb633f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "890f505bfd367129e3a4",
+    "url": "/static/css/main.9ff1ad5b.chunk.css"
+  },
+  {
+    "revision": "e9249e2b51681e13b9bd",
+    "url": "/static/js/2.83feb1a1.chunk.js"
+  },
+  {
+    "revision": "890f505bfd367129e3a4",
+    "url": "/static/js/main.9c06881c.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
